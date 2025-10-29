540abc2c0b3470c27befcd2a12fc878f7fea2a64
Syauqi <syauqi.d.u@gmail.com>
2025-10-29 17:56:05 +0700

    [FIX] Registrasi Hash

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
