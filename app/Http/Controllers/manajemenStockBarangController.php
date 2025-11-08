51919e99d6a3dc635d8a04e8e8cee0041cb865e1
Dafrisu <daffaraymisuandi@gmail.com>
2025-11-08 11:40:49 +0700

    Namabahin testcase untuk product management

--- app/Http/Controllers/manajemenStockBarangController.php
+++ app/Http/Controllers/manajemenStockBarangController.php
@@ -165,12 +165,12 @@ class manajemenStockBarangController extends Controller
 
             $product->delete();
 
-            ProductLogs::create([
-                'user_id' => Auth::user()->id,
-                'product_id' => $product->id,
-                'action' => 'delete',
-                'description' => 'Hapus product ' . $product->name,
-            ]);
+            // ProductLogs::create([
+            //     'user_id' => Auth::user()->id,
+            //     'product_id' => $product->id,
+            //     'action' => 'delete',
+            //     'description' => 'Hapus product ' . $product->name,
+            // ]);
 
             return response()->json([
                 'message' => 'Berhasil Hapus Product',
