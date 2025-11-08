4b87e76aebea1f8a57669aa794dd7966d88b532c
Kal EL Pratama <152152274+kalpratama@users.noreply.github.com>
2025-11-08 16:00:06 +0700

    Re-Initiated branch
    Copy perubahan di branch dahsboard supaya coverage sonar reset.

--- app/Models/ProductLogs.php
+++ app/Models/ProductLogs.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class   ProductLogs extends Model
 {
+    use HasFactory;
     protected $fillable = ['user_id', 'product_id', 'action', 'description'];
 
     public function user(): BelongsTo
